b12a286682504af34b2d81a2b94e157c4b5b6c3e
Thasheeee <thashee2003@gmail.com>
2026-03-29 10:24:07 +0530

    Align secrets with Hugging Face names

--- backend/server.py
+++ backend/server.py
@@ -23,9 +23,9 @@ mongo = PyMongo(app) #
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME') # Use the same secret
  
 mail = Mail(app)
 
