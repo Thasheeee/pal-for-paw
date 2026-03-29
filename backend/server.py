1ee93a73602b7c69ff12e583b4e8c0e3831dd54d
Thasheeee <thashee2003@gmail.com>
2026-03-30 04:12:39 +0530

    email

--- backend/server.py
+++ backend/server.py
@@ -25,9 +25,9 @@ mongo = PyMongo(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_USERNAME'] = "palforpaw@gmail.com"
+app.config['MAIL_PASSWORD'] = "gsmrtvhfqhiordgm"
+app.config['MAIL_DEFAULT_SENDER'] = "palforpaw@gmail.com"
  
 mail = Mail(app)
 
